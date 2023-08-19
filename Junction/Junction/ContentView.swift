7484f570f73a60bbf3a57b8871c914d6d0f2457d
보경 <97583162+bokoo14@users.noreply.github.com>
2023-08-19 21:17:23 +0900

    [Feat] 회원가입 및 자동 로그인 기능 구현, Splash 기능 설정

--- Junction/Junction/ContentView.swift
+++ Junction/Junction/ContentView.swift
@@ -8,9 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
-
+    @State var isLoading: Bool = true
+    @AppStorage("isLoggedIn") var isLoggedIn: Bool = UserDefaults.standard.bool(forKey: "isLoggedIn")
+    
     var body: some View {
-        Text("content View")
+        ZStack {
+            if isLoggedIn { // 로그인이 된 상태라면
+                MainView()
+            } else { // 로그인이 되지 않은 상태라면
+                VStack (spacing: 0){
+                    LoginView()
+                } .zIndex(0)
+            }
+            
+            if isLoading { // Splash
+                launchScreenView.transition(.opacity).zIndex(1)
+            }
+        } // ZStack
+        .onAppear {
+            autoLogin()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5, execute: {
+                withAnimation { isLoading.toggle() }
+            })
+        } // onAppear
+    }
+    
+    func autoLogin() {
+        if let userID = UserDefaults.standard.string(forKey: "userID") {
+            if let password = UserDefaults.standard.string(forKey: "userID") {
+                sendPostRequest("\(url)users/signin", parameters: ["userID": userID, "password": password]){
+                    responseObject, error in guard let _ = responseObject, error == nil else {
+                        print(error ?? "Unknown error")
+                        return
+                    }
+                }
+            }
+        }
+    } // func autoLogin
+}
+
+
+extension ContentView {
+    var launchScreenView: some View {
+        ZStack (alignment: .center){
+            LinearGradient(gradient: Gradient(colors: [.red, .blue]),
+                           startPoint: .top, endPoint: .bottom)
+            .edgesIgnoringSafeArea(.all)
+        }
     }
 }
 
--- Junction/Junction/View/Login/SignInView.swift
+++ Junction/Junction/View/Login/SignInView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+let url: String = "https://fcea-110-70-54-54.ngrok-free.app/"
+
 struct SignInView: View {
     @State var userName: String = ""
     @State var userID: String = ""
@@ -14,8 +16,10 @@ struct SignInView: View {
     @State var userPasswordCheck: String = ""
     @State var isClicked: Bool = false
     
+    
     var body: some View {
         VStack (alignment: .leading, spacing: 0){
+            Text("SignIn")
             // MARK: 사용자 이름
             Text("user의 이름: \(userName)")
             TextField("이름을 입력해주세요!", text: $userName)
@@ -36,7 +40,7 @@ struct SignInView: View {
             
             // MARK: 비밀번호
             Text("user의 비밀번호: \(userPassword)")
-            SecureField("비밀번호를 입력하세요", text: $userPassword)
+            TextField("비밀번호를 입력하세요", text: $userPassword)
                 .frame(width: 300, height: 10)
                 .padding()
                 .background(Color(.systemGray6))
@@ -45,7 +49,7 @@ struct SignInView: View {
             
             // MARK: 비밀번호 확인
             
-            SecureField("비밀번호를 다시 입력하세요", text: $userPasswordCheck)
+            TextField("비밀번호를 다시 입력하세요", text: $userPasswordCheck)
                 .frame(width: 300, height: 10)
                 .padding()
                 .background(Color(.systemGray6))
@@ -60,17 +64,17 @@ struct SignInView: View {
                 isClicked = true
                 
                 // 회원가입 POST로직
-//                sendPostRequest("<http://localhost:8000/auth/signup>", parameters:
-//                                    ["userName": self.userName, "useID": self.userID, "password": self.userPassword]
-//                ){
-//                    responseObject, error in guard let _ = responseObject, error == nil else {
-//                        print(error ?? "Unknown error")
-//                        return
-//                    }
-//                }
-                
+                sendPostRequest("\(url)users/signup", parameters:
+                                    ["userName": self.userName, "userID": self.userID, "password": self.userPassword]
+                ){
+                    responseObject, error in guard let _ = responseObject, error == nil else {
+                        print(error ?? "Unknown error")
+                        return
+                    }
+                    
+                }
             } label: {
-                Text("회원가입 로직")
+                Text("회원가입")
             }.disabled(userPassword != userPasswordCheck)
         }
         .padding(20)
